00266484e213abc31358e931a05a7089520dd3c6
徐志强 <652732310@qq.com>
2021-03-28 19:32:22 +0800

    log

--- p2pserver/peer/peer.go
+++ p2pserver/peer/peer.go
@@ -360,10 +360,7 @@ func (this *Peer) SendRaw(msgType string, msgPayload []byte, isConsensus bool) e
 	this.connLock.Lock()
 	defer func() {
 		this.connLock.Unlock()
-		duration := time.Now().Sub(start)
-		if duration > time.Millisecond*100 {
-			log.Infof("[p2p] SendRaw took %s, payload length %d addr %s", time.Now().Sub(start).String(), len(msgPayload), this.GetAddr())
-		}
+		log.Infof("[p2p] SendRaw took %s, payload length %d addr %s", time.Now().Sub(start).String(), len(msgPayload), this.GetAddr())
 	}()
 
 	if isConsensus && this.ConsLink.Valid() {
